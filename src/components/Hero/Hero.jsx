7fcb6562e8fa146a3f034299ea07707d279c1b6e
Carlos Augusto Prado <devcarlosaugustoprado@gmail.com>
2023-03-21 14:37:43 -0300

    criação da section hero e alterações no header

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Hero.scss";
+import photo from "./programming.jpg";
+
+const Hero = () => {
+  return (
+    <>
+      <section className="s-hero">
+        <div className="container">
+          <div className="titles-area">
+            <h2>Olá, sou o</h2>
+            <h1>Carlos Augusto Prado</h1>
+            <p>
+              Sou um desenvolvedor front-end / desenvolvedor full-stack <br />{" "}
+              com foco em ReactJS. Entusiasta de novas tecnologias e apaixonado
+              pelo front-end em geral.
+            </p>
+          </div>
+          <div className="image-area">
+            <img src={photo} alt="" />
+          </div>
+        </div>
+      </section>
+      <section className="black-tiles"></section>
+    </>
+  );
+};
+
+export default Hero;
